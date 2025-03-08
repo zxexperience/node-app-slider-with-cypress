d9c968f67ea2cc441bd788eaad544651b4e8a5c3
zxexperience <33825976+zxexperience@users.noreply.github.com>
2025-03-08 12:50:07 +0100

    Update slider.cy.js
    Added two new tests

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -7,6 +7,20 @@ describe('Swiper Gallery Test', function () {
 });
 
 describe('Swiper Gallery Test', function () {
+  it('Check all slides', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.wait(2000);
+    cy.get('.swiper-button-next').click({ force: true });
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});
+
+describe('Swiper Gallery Test', function () {
   it('Checks if third slide contains "Paris"', function () {
     cy.visit('http://localhost:3000');
     cy.get('.swiper-button-next').click();
@@ -16,3 +30,12 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if previous slide contains "Paris"', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-prev').click();
+    cy.get('.swiper-slide-active').should('contain', 'Paris');
+  });
+});
+
